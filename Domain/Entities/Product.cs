319afc45842776f2e1b50ad7c6f605bc85d2cb22
lansrod <lansrod@DESKTOP-HKJ8QEN>
2023-01-09 15:45:07 +0100

    Ajoutez des fichiers projet.

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Barcode { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Rate { get; set; }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,59 @@
+using Application;
+using Persistence;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddApplication();
+            builder.Services.AddPersistence(builder.Configuration);
+            builder.Services.AddSwaggerGen(setup =>
+            {
+                setup.IncludeXmlComments(string.Format(@"{0}\WebApi.xml", AppDomain.CurrentDomain.BaseDirectory));
+                setup.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "OnionArchitecture",
+                });
+            });
+            builder.Services.AddApiVersioning(config =>
+            {
+                // Specify the default API Version as 1.0
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                // If the client hasn't specified the API version in the request, use the default API version number 
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
